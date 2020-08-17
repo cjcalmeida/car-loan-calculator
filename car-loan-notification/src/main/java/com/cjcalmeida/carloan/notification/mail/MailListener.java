9a52b7e454d123dcaeae4147cb81fd5ce9a3b35c
Cristiano Almeida <cicbboyzb@gmail.com>
2020-08-17 02:06:58 -0300

    refactor(notifications): Some improvements

--- car-loan-notification/src/main/java/com/cjcalmeida/carloan/notification/mail/MailListener.java
+++ car-loan-notification/src/main/java/com/cjcalmeida/carloan/notification/mail/MailListener.java
@@ -1,13 +1,14 @@
-package com.cjcalmeida.carloan.notification.mail.listener;
+package com.cjcalmeida.carloan.notification.mail;
 
+import com.cjcalmeida.carloan.core.events.LoanEvent;
 import com.cjcalmeida.carloan.notification.domain.Mail;
 import com.cjcalmeida.carloan.notification.exception.TemplateNotFoundException;
-import com.cjcalmeida.carloan.notification.mail.MailService;
 import com.cjcalmeida.carloan.notification.template.TemplateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 import javax.mail.MessagingException;
 import javax.validation.Valid;
@@ -17,7 +18,7 @@ import javax.validation.constraints.NotNull;
 import java.util.Map;
 import java.util.UUID;
 
-@EnableBinding(MailBinding.class)
+@EnableBinding(Sink.class)
 class MailListener {
 
     private final Logger log = LoggerFactory.getLogger(MailListener.class);
@@ -30,47 +31,32 @@ class MailListener {
         this.mailService = mailService;
     }
 
-    @StreamListener(MailBinding.INPUT)
-    public void sendMail(@Valid MailSchema mailSchema) throws TemplateNotFoundException, MessagingException {
-        log.info("Receiving command to send mail");
+    @StreamListener(Sink.INPUT)
+    public void sendMail(@Valid LoanEvent<ProposalSavedEnvelope> event) throws TemplateNotFoundException, MessagingException {
+        log.info(event.printMetadata());
+        var notificationSchema = event.getData().notification;
 
-        var template = templateService.getTemplate(mailSchema.templateName);
+        var template = templateService.getTemplate(notificationSchema.templateName);
         log.debug("Using template: {}-{}", template.getId(), template.getName());
 
-        var mail = new Mail(mailSchema.id, mailSchema.subject,  mailSchema.to, template);
-        mail.setParams(mailSchema.args);
+        var mail = new Mail(notificationSchema.id, notificationSchema.subject,  notificationSchema.to, template);
+        mail.setParams(notificationSchema.args);
         mailService.sendEmail(mail);
+    }
+
+    static class ProposalSavedEnvelope {
+        public MailSchema notification;
     }
 
     static class MailSchema {
         @NotNull
-        UUID id;
+        public UUID id;
         @NotNull @NotEmpty
-        String subject;
+        public String subject;
         @NotNull @Email
-        String to;
+        public String to;
         @NotNull @NotEmpty
-        String templateName;
-        Map<String, Object> args;
-
-        public void setId(UUID id) {
-            this.id = id;
-        }
-
-        public void setSubject(String subject) {
-            this.subject = subject;
-        }
-
-        public void setTo(String to) {
-            this.to = to;
-        }
-
-        public void setTemplateName(String templateName) {
-            this.templateName = templateName;
-        }
-
-        public void setArgs(Map<String, Object> args) {
-            this.args = args;
-        }
+        public String templateName;
+        public Map<String, Object> args;
     }
 }
